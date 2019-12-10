d427ec5859d943d42fdd5b353ead9e56c1847314
Wesiem Bouzaiane <wesiem.bouzaiane@gmail.com>
2019-12-10 14:43:48 +0100

    Added functionality to register votes.

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Mood;
+use App\Vote;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -35,12 +37,26 @@ class HomeController extends Controller
             case "happy":
             case "unemotional":
             case "unhappy":
-                return redirect('thank-you');
+                // get the id of the mood based on its slug
+                $mood = Mood::where('slug', $voteslug)->get();
+                $mood_id = $mood[0]->id;
 
-            default:
-                return redirect('');
-                break;
+                 // save vote
+                if ($mood_id > 0) {
+                    $mood = new Vote;
+                    $mood->mood_id = $mood_id;
+                    $mood->datetime = date("Y-m-d H:i:s");
+                    $mood->save();
+
+                    // NOTE: Here the vote could be verified to be sure it has been registered. But for now we'll leave it as it is.
+
+                    // redirect to thank you page
+                    return redirect('thank-you');
+                }
         }
+
+        // redirect to the homepage
+        return redirect('');
     }
 
     /**
