8dd82e8a739ccf84ce4c562aa0d3b5d8e6951276
Wesiem Bouzaiane <wesiem.bouzaiane@gmail.com>
2019-12-10 12:42:55 +0100

    Add database moods table

---
+++ database/migrations/2019_12_10_113458_create_moods_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMoodsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('moods', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name', 20);
+            $table->string('slug', 20);
+            $table->string('emoji', 5);
+            $table->dateTime('created_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('moods');
+    }
+}
